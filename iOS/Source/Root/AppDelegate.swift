8f65e9170c1145602f78ce92a98d467b5c3b027d
Elvis Nuñez <elvisnunez@me.com>
2016-10-23 17:42:36 +0200

    first commit

---
+++ iOS/Source/Root/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+    var window: UIWindow?
+
+    lazy var fetcher: Fetcher = {
+        let fetcher = Fetcher(baseURL: "https://server.com", modelName: "DataModel")
+
+        return fetcher
+    }()
+}
+
+extension AppDelegate: UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window = self.window else { fatalError("Window not found") }
+
+        window.rootViewController = ItemsController(fetcher: self.fetcher)
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
